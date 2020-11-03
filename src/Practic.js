23691fc8d6aafa2479f7ed98e7f0f2739ca5dc56
Baleria Ospina Monsalve <65976493+BaleriaOspina@users.noreply.github.com>
2020-11-03 17:20:28 -0500

    Update Practic.js

--- src/Practic.js
+++ src/Practic.js
@@ -7,3 +7,12 @@ let edad = "19";
 console.log(`Hola mi nombre es: ${nombre} ${apellido} y tengo ${edad} años`);
 
 //función
+
+let num = 22;
+
+function multip(valor) {
+  return valor * valor;
+}
+
+let resultado = multip(6) * num;
+console.log(resultado);
