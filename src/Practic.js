2161c0fe2e909e3d9e9192c18667fb4cf821dcd1
Baleria Ospina Monsalve <65976493+BaleriaOspina@users.noreply.github.com>
2020-11-03 16:45:12 -0500

    Initial commit

---
+++ src/Practic.js
@@ -0,0 +1,9 @@
+//Definir variables
+
+let nombre = "Baleria";
+let apellido = "Ospina";
+let edad = "19";
+
+console.log(`Hola mi nombre es: ${nombre} ${apellido} y tengo ${edad} años`);
+
+//función
